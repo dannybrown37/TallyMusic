07916e76548a5a8b65bbf40bf3331db6f793a93f
Danny Brown <dannybrown37@gmail.com>
2018-12-09 10:47:27 -0500

    more fixes to allowed hosts

--- tallymusic/settings.py
+++ tallymusic/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['tallymusic.herokuapp.com', 'tallymusic.net']
+ALLOWED_HOSTS = ['.herokuapp.com', '.tallymusic.net']
 
 # Application definition
 INSTALLED_APPS = [
