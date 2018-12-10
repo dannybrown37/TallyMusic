8197afa43f5f30e11c42b44c0a70b31a8086e646
Danny Brown <dannybrown37@gmail.com>
2018-12-10 08:45:29 -0500

    new day, new attempt

--- tallymusic/settings.py
+++ tallymusic/settings.py
@@ -20,10 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = [
